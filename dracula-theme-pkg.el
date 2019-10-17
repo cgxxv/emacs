4d1bba7511a82c793ed11c900f2ff5d80fa7d7ab
cgxxv <cghack@yeah.net>
2019-10-17 19:36:46 +0800

    support for spacemacs

---
+++ dracula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dracula" "20191017.1843" "dracula Color Theme" 'nil :commit "630173873504376e6784d134f90beaaf615bbc9c" :keywords '("color" "theme" "dracula") :url "https://github.com/dracula/emacs/dracula-theme.el")
